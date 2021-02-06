dc4816534645e94d0aa74ccf63de3086296d36f0
leonyangelaa <68493516+leonyangelaa@users.noreply.github.com>
2021-02-07 01:47:27 +0700

    Update Blinking Sequencely without Push Button.ino

--- Project #2/Digital IO/Blinking Sequencely without Push Button.ino
+++ Project #2/Digital IO/Blinking Sequencely without Push Button.ino
@@ -13,7 +13,7 @@ void setup() {
   // initialize the digital pin as an output.
   pinMode(led1, OUTPUT);   
   pinMode(led2, OUTPUT);  
-  pinMode(led3, OUTPUT);    
+  pinMode(led3, OUTPUT);
   pinMode(led4, OUTPUT);
 }
 
